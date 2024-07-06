b77da60c9a12be50338035e6a18154d61bb90fa5
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-07-06 10:03:32 +0530

    Update PaymentLinkResponse.java

--- ecommerce/src/main/java/com/ebaad/ecommerce/response/PaymentLinkResponse.java
+++ ecommerce/src/main/java/com/ebaad/ecommerce/response/PaymentLinkResponse.java
@@ -1,1 +1,31 @@
+package com.ebaad.ecommerce.response;
+
+public class PaymentLinkResponse {
+    private String payment_link_url;
+    private String payment_link_id;
+
+    public PaymentLinkResponse() {
+    }
+
+    public PaymentLinkResponse(String payment_link_url, String payment_link_id) {
+        this.payment_link_url = payment_link_url;
+        this.payment_link_id = payment_link_id;
+    }
+
+    public String getPayment_link_url() {
+        return payment_link_url;
+    }
+
+    public void setPayment_link_url(String payment_link_url) {
+        this.payment_link_url = payment_link_url;
+    }
+
+    public String getPayment_link_id() {
+        return payment_link_id;
+    }
+
+    public void setPayment_link_id(String payment_link_id) {
+        this.payment_link_id = payment_link_id;
+    }
+}
 
